22daeb8cffa21386590b0b283088838b3b604b4a
sreekanthramisetty <55994651+sreekanthramisetty@users.noreply.github.com>
2020-05-11 08:33:16 +0530

    Break

---
+++ Break.py
@@ -0,0 +1,21 @@
+# x = int(input("ENTER THE ITEM QUANTITY: "))
+# av = 5
+# i: int = 1
+# while i <= x:
+#     if i > av:
+#         print(i,"and above is Out Of Stock")
+#         break
+#     print("apple:", i)
+#     i+=1
+
+
+# for i in range(1,101):
+#     if (i%3 == 0 or i%5 == 0):
+#         continue
+#     print(i)
+
+# for i in range(1,101):
+#     if (i%3 == 0 or i%5 == 0):
+#         pass
+#     else:
+#      print(i)
